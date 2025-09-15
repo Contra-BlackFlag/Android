333992e1b42333a98adbd726f8b4254509001222
Piyush Vishwakarma <pvvishwa12345@gmail.com>
2025-09-15 20:08:55 +0530

    New code

--- WishlistApp/app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ WishlistApp/app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -2,9 +2,13 @@ package com.example.wishlistapp
 
 import android.R
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
@@ -16,6 +20,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.ui.text.font.FontWeight
+import com.example.wishlistapp.Data.DummyWish
 import com.example.wishlistapp.Data.Wish
 
 @Composable
@@ -36,12 +44,27 @@ fun HomeView(){
         }
     ){
         LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
+                items(DummyWish.wishList){
+                    wish-> WishItem(wish = wish) {
 
+                }
+                }
         }
     }
 }
 
 @Composable
 fun WishItem(wish : Wish, onClick : ()-> Unit){
-    Card() {  }
+    Card(modifier = Modifier.fillMaxWidth()
+        .padding(top = 8.dp, start = 10.dp, end = 10.dp)
+        .clickable{
+            onClick()
+        },
+        elevation = 15.dp,
+        backgroundColor = Color.White
+        ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)        }
+            }
 }
