549282d81137b1af5a74e314606702a57b072360
Piyush Vishwakarma <pvvishwa12345@gmail.com>
2025-09-16 21:13:24 +0530

    New code

--- ToDo/app/src/main/java/com/example/todo/ShoppingItem.kt
+++ ToDo/app/src/main/java/com/example/todo/ShoppingItem.kt
@@ -1,5 +1,10 @@
 package com.example.shoppinglist
 
+import android.Manifest
+import android.content.Context
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -18,6 +23,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -35,17 +41,61 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.navigation.NavController
+import com.example.todo.LocationUtil
+import com.example.todo.LocationViewModel
+import com.example.todo.MainActivity
 import kotlinx.serialization.internal.throwMissingFieldException
 
-data class ShoppingItem(val id:Int, var name:String, var quantity:Int, var IsEditing : Boolean = false){
+data class ShoppingItem(val id:Int,
+                        var name:String,
+                        var quantity:Int,
+                        var IsEditing : Boolean = false,
+                        var address : String = ""){
 
 }
 @Composable
-fun ShoppingList(){
+fun ShoppingList(
+    locationUtils : LocationUtil,
+    viewModel: LocationViewModel,
+    navController: NavController,
+    context: Context,
+    address: String
+){
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var ShowDialog by remember { mutableStateOf(false) }
     var Name by remember { mutableStateOf("") }
     var IQuantity by remember { mutableStateOf("") }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult( contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION]  == true && permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true){
+
+
+            }
+            else{
+                val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                if (rationaleRequired){
+                    Toast.makeText(context,
+                        "Location Needed",
+                        Toast.LENGTH_LONG).show()
+                }
+                else {
+                    Toast.makeText(context,
+                        "Setting->More Networks->Location",
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
 
         Button(onClick = { ShowDialog = true}, modifier = Modifier
@@ -142,8 +192,18 @@ fun ShoppingItemList(item: ShoppingItem,onEditClick : ()->Unit,onDeleteClick : (
     Row(
         modifier = Modifier.padding(8.dp).fillMaxWidth().border(border = BorderStroke(4.dp, Color.Black), shape  = RoundedCornerShape(30) )
     ) {
-        Text(text = item.name, modifier = Modifier.padding(8.dp))
-        Text(text = "Qty : ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Column(
+            modifier = Modifier.weight(1f).padding(8.dp)
+        ) {
+            Row {
+                Text(text = item.name, modifier = Modifier.padding(8.dp))
+                Text(text = "Qty : ${item.quantity}", modifier = Modifier.padding(8.dp))
+            }
+            Row(Modifier.fillMaxWidth()) {
+                Icon(imageVector = Icons.Default.LocationOn, contentDescription = null)
+                Text(text = item.address)
+            }
+        }
         Row(modifier = Modifier.padding(8.dp)) {
             IconButton(onClick = onEditClick) {
                 Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit Button")
